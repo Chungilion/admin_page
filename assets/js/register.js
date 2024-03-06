811bb64474fe07d8318f9725608cc5b9f037df6c
Chungilion <fileapotoxin4869@gmail.com>
2024-03-06 07:45:55 +0700

    Update register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const email = document.getElementById('register__user').value;
         const password = document.getElementById('register__pw').value;
         
-        if (validateEmail(email) === false) {
+        if (validateEmail(document.getElementById('register__user')) === false) {
           loginStatusNoti(false, 'Email không hợp lệ.');
           return;
         }
